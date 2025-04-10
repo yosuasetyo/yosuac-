d8d2c5e9add4ab5c74013d7133420a656ddc19b8
yosuasetyo <yosuasetyo07@gmail.com>
2025-04-11 00:40:46 +0700

    Add files via upload

---
+++ yosua tugas algoritma c++.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string nama,kelas,NIM,hobi;
+    cout<<"masukkan nama:"<<endl;
+    cin>> nama;
+
+    cout<<"masukkan kelas"<<endl;
+    cin>> kelas;
+
+    cout<<"masukkan NIM"<<endl;
+    cin>> NIM;
+
+    cout<<"masukkan hobi"<<endl;
+    cin>> hobi;
+
+    cout<<"==Data diri=="<<endl;
+    cout<<"nama:"<<nama<<endl;
+    cout<<"kelas:"<<kelas<<endl;
+    cout<<"NIM:"<<NIM<<endl;
+    cout<<"hobi:"<<hobi<<endl;
+}
